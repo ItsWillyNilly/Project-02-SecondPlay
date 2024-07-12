e936555b3abe92123b65be5b3bdbe43362ee999e
faizahaque90 <163949218+faizahaque90@users.noreply.github.com>
2024-07-11 21:32:30 -0700

    deleted all the content inside the files

--- seeds/userdata.js
+++
@@ -1,7 +0,0 @@
-const{User} = require("../models");
-const userData = [{
-    username: "FaizaHaque",
-    password:"helloo",
-}];
-const seedUsers = () => User.bulkCreate(userData, {individualHooks: true});
-module.exports = seedUsers;
