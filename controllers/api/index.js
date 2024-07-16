7e0e68f145cdd267eaf36f5b04018139dc03f1cf
robel <robelabraha11@gmail.com>
2024-07-15 21:55:12 -0700

    update changes

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userRoutes = require('./user');
+
+// Add other routes here as needed
+// const productRoutes = require('./product');
+
+router.use('/users', userRoutes);
+// router.use('/products', productRoutes);
+
+module.exports = router;
