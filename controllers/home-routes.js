9d8955cfc7eee402677602c5668a08d3fa8c65d4
faizahaque90 <163949218+faizahaque90@users.noreply.github.com>
2024-07-15 21:47:09 -0700

    work on post.js card-feed.handlebars and home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,33 @@
 const router = express.Router();
-
+const { Post, User } = require('../models');
 // route to get all products
-router.get('/', (req, res) => {
-
-});
+router.get('/', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const postData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+        order:[
+          ['created_at', 'DESC']
+        ]
+      });
+  
+      // Serialize data so the template can read it
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        posts, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 // route to get one product
 router.get()
---
+++ models/Post.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+class Post extends Model { }
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        personpostedby: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        condition: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "users",
+                key: "id"
+            }
+        }
+    }, {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post"
+});
+module.exports = Post;
