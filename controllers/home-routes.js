4e543e2fa7f3eff6a31925003c8bb9d3bab481d9
faizahaque90 <163949218+faizahaque90@users.noreply.github.com>
2024-07-18 18:02:26 -0700

    add the sign up respond and request route in the home page routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,30 +2,33 @@ const router = require("express").Router();
 const { Post, User } = require('../models');
 // route to get all products
 router.get('/', async (req, res) => {
-    try {
-      // Get all projects and JOIN with user data
-      const postData = await Post.findAll({
-     
-        order:[
-          ['created_at', 'DESC']
-        ]
-      });
-  
-      // Serialize data so the template can read it
-      const posts = postData.map((post) => post.get({ plain: true }));
-      console.log("postdata : >>\n", posts);
-      // Pass serialized data and session flag into template
-      res.render('home-page', { 
-        posts, 
-        loggedIn: req.session.loggedIn 
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  try {
+    // Get all projects and JOIN with user data
+    const postData = await Post.findAll({
+
+      order: [
+        ['created_at', 'DESC']
+      ]
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log("postdata : >>\n", posts);
+    // Pass serialized data and session flag into template
+    res.render('home-page', {
+      posts,
+      loggedIn: req.session.loggedIn
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get('/login', (req, res) => {
-    res.render('login');
+  res.render('login');
+});
+router.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
 // route to get one product
